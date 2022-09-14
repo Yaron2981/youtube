3a3d08a7982208eb8ca90803c6eb253980f8e15b
Yaron2981 <pilot2981@gmail.com>
2022-09-14 14:02:08 +0300

    Main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  showFiller: boolean = true;
+  drawer: boolean = true;
   title = 'youtube';
 }
