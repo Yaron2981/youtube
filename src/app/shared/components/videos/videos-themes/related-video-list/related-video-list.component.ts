d6d35df3c66b23be2019ff70f62c3b430e361d15
Yaron2981 <pilot2981@gmail.com>
2022-10-31 14:31:06 +0200

    speated the videos components

--- src/app/shared/components/videos/videos-themes/related-video-list/related-video-list.component.ts
+++ src/app/shared/components/videos/videos-themes/related-video-list/related-video-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Video } from 'src/app/search.interface';
+import { YOUTUBE_CONST } from 'src/app/shared/constants/yt';
 
 @Component({
   selector: 'app-related-video-list',
   templateUrl: './related-video-list.component.html',
-  styleUrls: ['./related-video-list.component.scss']
+  styleUrls: ['./related-video-list.component.scss'],
 })
 export class RelatedVideoListComponent implements OnInit {
-
-  constructor() { }
-
+  @Input('video') video: Video | null = null;
+  YT = YOUTUBE_CONST;
+  constructor() {}
   ngOnInit(): void {
+    console.log(this.video);
   }
-
 }
